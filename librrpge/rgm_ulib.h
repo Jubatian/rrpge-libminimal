7b9dabd1bf99b189411817b0e57b8a8bc6bed691
Sandor Zsuga <jubatian@jubatian.com>
2015-02-05 10:22:39 +0100

    User Library binary and data updated in accordance with tileset interface redesign.

--- librrpge/rgm_ulib.h
+++ librrpge/rgm_ulib.h
@@ -6,7 +6,7 @@
 **             License) extended as RRPGEvt (temporary version of the RRPGE
 **             License): see LICENSE.GPLv3 and LICENSE.RRPGEvt in the project
 **             root.
-**  \date      2015.02.03
+**  \date      2015.02.05
 **
 **
 **  The User Library included is taken from the RRPGE User Library project.
@@ -20,13 +20,13 @@
 
 
 /* Size of the RRPGE User Library */
-#define RRPGE_M_ULIB_SIZE (6884U / 2U)
+#define RRPGE_M_ULIB_SIZE (6860U / 2U)
 
 /* The RRPGE User Library */
 extern const uint16 rrpge_m_ulib[RRPGE_M_ULIB_SIZE];
 
 /* Count of elements in the 0xFA00 - 0xFAFF nonzero data initializers */
-#define RRPGE_M_ULIB_ICNT 27U
+#define RRPGE_M_ULIB_ICNT 43U
 
 /* 0xFA00 - 0xFAFF initializers. First byte is offset low, then high & low of data */
 extern const uint8  rrpge_m_ulib_idat[RRPGE_M_ULIB_ICNT * 3U];
